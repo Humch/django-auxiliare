a2568e0789c87f8ae6feeec0f8e575e530cb335d
Fabien Schlegel <fabienschlegel@yahoo.fr>
2017-01-24 10:42:48 +0100

    update menu

--- auxiliare/urls.py
+++ auxiliare/urls.py
@@ -2,7 +2,10 @@ from django.contrib.auth import views as auth_views
 
 from django.conf.urls import url
 
+from .views import HomeView
+
 urlpatterns = [
-    url(r'^accounts/login/$', auth_views.login, name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^$', HomeView.as_view(), name = 'aux_home' ),
+    url(r'^accounts/login/$', auth_views.login, name ='aux_login'),
+    url(r'^accounts/logout/$', auth_views.logout, name ='aux_logout', kwargs = {'next_page': '/'}),
 ]
--- auxiliare/views.py
+++ auxiliare/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import View
 
-# Create your views here.
+class HomeView(View):
+    
+    template_name='auxiliare/home.html'
+    
+    def get(self, request):
+        """
+        Get method for the home view of the project.
+        Modify menu according to installed apps
+        """
+        
+        if 'paperworks' in settings.INSTALLED_APPS:
+            paperworks = True
+        else:
+            paperworks = False
+        
+        if 'shoppinglist' in settings.INSTALLED_APPS:
+            shopping_list = True
+        else:
+            shopping_list = False
+            
+        return render(request, self.template_name,{'paperworks':paperworks,'shopping_list':shopping_list})
+    
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(HomeView, self).dispatch(*args, **kwargs)
