f48004acb1a37d705c97548da990239f4727c3a6
Fabien Schlegel <fabienschlegel@yahoo.fr>
2017-02-22 15:35:35 +0100

    update menu, link appear if app is in installed apps

--- auxiliare/urls.py
+++ auxiliare/urls.py
@@ -2,10 +2,11 @@ from django.contrib.auth import views as auth_views
 
 from django.conf.urls import url
 
-from .views import HomeView
+from .views import HomeView, MenuView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name = 'aux_home' ),
+    url(r'^menu/$', MenuView.as_view(), name = 'aux_menu' ),
     url(r'^accounts/login/$', auth_views.login, name ='aux_login'),
     url(r'^accounts/logout/$', auth_views.logout, name ='aux_logout', kwargs = {'next_page': '/'}),
 ]
--- auxiliare/views.py
+++ auxiliare/views.py
@@ -7,7 +7,6 @@ from django.views.generic import View
 class HomeView(View):
     """
     Render for the home view of the project.
-    Modify menu according to installed apps
     """
 
     template_name='auxiliare/home.html'
@@ -19,3 +18,29 @@ class HomeView(View):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(HomeView, self).dispatch(*args, **kwargs)
+
+installed_apps = getattr(settings, 'INSTALLED_APPS','')
+    
+class MenuView(View):
+    """
+    Render for the menu view of the project.
+    Modify menu according to installed apps
+    """
+
+    template_name='menu.html'
+    
+    def get(self, request):
+        
+        if 'paperworks' in installed_apps:
+
+            paperworks_menu = True
+        
+        if 'simplecost' in installed_apps:
+            
+            simplecost_menu = True
+            
+        return render(request, self.template_name, {'paperworks_menu' : paperworks_menu, 'simplecost_menu' : simplecost_menu})
+    
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(MenuView, self).dispatch(*args, **kwargs)
